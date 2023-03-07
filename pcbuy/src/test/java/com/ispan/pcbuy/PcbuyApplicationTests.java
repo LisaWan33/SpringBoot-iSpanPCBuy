f82b176f58c1f3a5dfeed397a06f63ef3f14ece3
LisaWan <lisayes396@gmail.com>
2023-03-07 15:27:37 +0800

    ispanPCBUY 電腦主機零組件購物商城/終

---
+++ pcbuy/src/test/java/com/ispan/pcbuy/PcbuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ispan.pcbuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcbuyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
